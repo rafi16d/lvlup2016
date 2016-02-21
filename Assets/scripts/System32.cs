9a9297c89bd2a3d6984d5cc0dd189b8581116fd5
Alaanor <max.bonvin@gmail.com>
2016-02-21 19:20:42 +0100

    fix music stop on hit Sys32

--- Assets/scripts/System32.cs
+++ Assets/scripts/System32.cs
@@ -37,6 +37,7 @@ public class System32 : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.tag == "Player") {
             userinterface.SetActive(false);
+            GameObject.Find("_Manager").SendMessage("stopMusic");
             GameObject.Find("Player").SendMessage("stopSound");
             GameObject.Find("Player").SendMessage("sleepPlayer", Mathf.Infinity);
             //GameObject.Find("_Manager").SendMessage("upgradeMusic");
