a331e5d2d4616815a926253b219b76cc78952bc2
Raphaël Gabriel <info@flopy.ch>
2016-02-25 19:21:55 +0100

    Minor fixe + scene Level

---
+++ Assets/scripts/mainmenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainmenu : MonoBehaviour {
+
+    public Toggle btntoggle;
+    public string level;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (btntoggle.isOn) {
+                SceneManager.LoadScene("introduction");
+            } else {
+                SceneManager.LoadScene(level);
+
+            }
+
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            if (btntoggle.isOn) {
+                SceneManager.LoadScene("introduction");
+            } else {
+                SceneManager.LoadScene(level);
+
+            }
+
+        }
+    }
+}
